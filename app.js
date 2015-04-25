4c84e24ca8258e3398749482c244ca47ea1df811
Jose Luis Orozco <handark@gmail.com>
2015-04-25 15:20:10 -0500

    Agregando soporte para nodejs

--- app.js
+++ app.js
@@ -5,8 +5,28 @@ var static = require('node-static');
 
 var fileServer = new static.Server('./IntranetIush');
 
+var puerto = normalizePort(process.env.PORT || '3000');
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
 require('http').createServer(function (request, response) {
     request.addListener('end', function () {
         fileServer.serve(request, response);
     }).resume();
-}).listen(3000);
+}).listen(puerto).on('listening', function(){
+	console.log("Intranet de IUSH corriendo en el puerto: " + puerto);
+});
